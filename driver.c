947fd5043d9f817af3368f965bbf68ce4988c2fc
devil <m.charly3015@gmail.com>
2016-09-02 02:23:09 +0530

    commit2

--- driver.c
+++ driver.c
@@ -3,7 +3,19 @@
 
 int main(int argc, char const *argv[])
 {
-	printf("working\n");
+
+	FILE *fp;
+	printf("%s working\n",argv[1]);
+	
+
+	fp=fopen(argv[1],"r");
+	if(!fp)
+	{
+		perror("Error : ");
+		return (-1);
+	}
+
+	fclose(fp);
 
 	return 0;
 }
