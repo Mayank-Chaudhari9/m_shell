623efd176ac8e2ed5c59e91b3d55c50f7fa21592
devil <m.charly3015@gmail.com>
2016-09-03 03:14:06 +0530

    night commit

--- driver.c
+++ driver.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 void parse(FILE *f_ptr);
 
@@ -37,19 +39,69 @@ void parse(FILE *f_ptr)
 {
 
 	FILE *fp1=f_ptr; 
-	char *line = NULL;
+	char *line = NULL,*read_l,c;
 	size_t len = 0;
 	ssize_t read;
+	int error=0,b_flag=0;
 
 	// while loop to read the file line by line and removing the comments
 
 	while ((read = getline(&line,&len,fp1)) != -1)
 	{
 		printf("Retrived line of length %zu :\n",read);
-		if(*line=='#')
+		read_l=(char *)malloc(sizeof(char)*read);
+		read_l=line;
+		c=*read_l;
+		while(isspace(c))
+			{
+				read_l=read_l+1;
+				c=*read_l;
+			}
+		//ignore lines starting with #
+		if(*read_l=='#')
 			continue;
-		printf("%s\n",line);
+		
+		//remove blank lines
+		if(read<=1)
+			continue;
+
+		if(read>1)
+		{
+			
+
+			if(strstr(read_l,"%BEGIN")&&(b_flag==0))
+				{
+					printf("BEGIN Found\n");
+					b_flag=1;
+					error=1;
+					//printf("%s\n",read_l);
+				}
+
+			if(b_flag==1)
+				{
+					if(strstr(read_l,"%BEGIN"))
+						{
+							continue;
+						}
+					printf("%s\n",read_l);	
+				}
+
+			if(strstr(read_l,"%END")&&(b_flag==1))
+			{
+				b_flag=0;
+				error=0;
+			}
+			
+		}
+		
+
+		//printf("%s\n",line);
 	}
+	if(error)
+		{
+			printf("Batch file finished abruptly\n");
+		}
+
 
 	free(line);
 
