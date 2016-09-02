deadc4070d9a3bb6669134d613f28cd842e95ce1
devil <m.charly3015@gmail.com>
2016-09-02 11:51:35 +0530

    third commit

--- driver.c
+++ driver.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void parse(FILE *f_ptr);
+
 int main(int argc, char const *argv[])
 {
 
@@ -14,8 +16,41 @@ int main(int argc, char const *argv[])
 		perror("Error : ");
 		return (-1);
 	}
+	
+	/**
+	*	Operations to control processing
+	*/
+	parse(fp);
+
 
 	fclose(fp);
 
 	return 0;
+}
+
+/**
+*
+*Function to parse the file line by line.
+*
+**/
+void parse(FILE *f_ptr)
+{
+
+	FILE *fp1=f_ptr; 
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	// while loop to read the file line by line and removing the comments
+
+	while ((read = getline(&line,&len,fp1)) != -1)
+	{
+		printf("Retrived line of length %zu :\n",read);
+		if(*line=='#')
+			continue;
+		printf("%s\n",line);
+	}
+
+	free(line);
+
 }
